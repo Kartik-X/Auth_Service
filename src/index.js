8e6fd7a6c99d9b1edbacc2575fceab02d1c767fe
kartik589123 <meta619012@gmail.com>
2023-06-07 10:29:34 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
 const app = express();
 
-// const { User } = require("./models/index");
-// const bcrypt = require("bcrypt");
-const UserRepository = require("./repository/user-repository");
+//const UserService = require("./services/user-service");
 
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
@@ -14,12 +12,13 @@ const prepareAndStartServer = () => {
   app.use("/api", apiRoutes);
   app.listen(PORT, async () => {
     console.log(`Server started on port: ${PORT}`);
-    const repo = new UserRepository();
-    const response = await repo.getById(3);
-    console.log(response);
-    // const incomingpassword = "12345";
-    // const user = await User.findByPk(3);
-    // const response = bcrypt.compareSync(incomingpassword, user.password);
+
+    const service = new UserService();
+    // const newToken = service.createToken({ email: "kk123@gmail.com", id: 1 });
+    // console.log("New token is", newToken);
+    // const token =
+    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtrMTIzQGdtYWlsLmNvbSIsImlkIjoxLCJpYXQiOjE2ODYxMTM0MjAsImV4cCI6MTY4NjExMzQ1MH0.KqjDB0dL8dZzySHsvhF3iU70BcdLW3GJV9WPNlSs-5s";
+    // const response = service.verifyToken(token);
     // console.log(response);
   });
 };
